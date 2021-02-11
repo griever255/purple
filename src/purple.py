7fe160a7a4fb115a38c2f74bce524742b4bf83b8
Joe <jgargiulo95@gmail.com>
2021-02-10 20:49:57 -0500

    unshuffled deck printed

--- src/purple.py
+++ src/purple.py
@@ -4,7 +4,7 @@ class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
-        
+
     def show(self):
         print("{} of {}".format(self.rank, self.suit))
 
@@ -16,4 +16,17 @@ class Deck:
     def initialize(self):
         for r in range(1,14):
             for s in ["Spades", "Clubs", "Hearts", "Diamonds"]:
+                correction = {
+                    1: "Ace",
+                    11: "Jack",
+                    12: "Queen",
+                    13: "King",
+                }
+                r = correction.get(r, r)
                 self.cards.append(Card(r,s))
+    def show(self):
+        for c in self.cards:
+            c.show()
+
+deck = Deck()
+deck.show()
